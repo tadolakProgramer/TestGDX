f412bcc445dfb54d191ba65a53a5758eccaedaf3
Tadusz Maziarz <tadolak@gmail.com>
2019-09-20 08:17:52 +0200

    Correct DesktopLauncher

--- desktop/src/com/tadolak/game/desktop/DesktopLauncher.java
+++ desktop/src/com/tadolak/game/desktop/DesktopLauncher.java
@@ -2,7 +2,9 @@ package com.tadolak.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.tadolak.game.MyGdxGame;
+
+
+import game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
