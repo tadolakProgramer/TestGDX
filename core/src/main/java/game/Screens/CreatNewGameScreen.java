04832cebb86239241bed7346197e340ac041464d
Tadeusz <tadolak@gmail.com>
2019-10-01 08:44:24 +0200

    rebuild table

--- core/src/main/java/game/Screens/CreatNewGameScreen.java
+++ core/src/main/java/game/Screens/CreatNewGameScreen.java
@@ -11,8 +11,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-import java.io.IOException;
-
 import game.HelperXML.MakeXML;
 import game.MyGdxGame;
 
@@ -20,10 +18,11 @@ public class CreatNewGameScreen extends AbstractScreen {
 
 
     protected Skin skin;
+    private static float padSize =25;
     private Table tablePlayer;
     private TextField tfName;
 
-    private String playerName;
+    private String CorporationName;
     private MyGdxGame game;
     private double money;
     private String locRoot;
@@ -45,15 +44,15 @@ public class CreatNewGameScreen extends AbstractScreen {
 
         tablePlayer = new Table();
 
-        tablePlayer.top().center().pad(10f);
-        //tableCrows.debug();
+        tablePlayer.top().center().pad(padSize);
+        tablePlayer.debug();
         tablePlayer.setFillParent(true);
 
         tablePlayer.row();
-        tablePlayer.add(new Label("Your Name Captein:", skin)).pad(10);
-        TextField tfName = new TextField("Capitan Solo", skin);
-        playerName = tfName.getText();
-        tablePlayer.add(tfName);
+        tablePlayer.add(new Label("Your Corporation name:", skin)).pad(padSize);
+        TextField tfName = new TextField("Space Trading", skin);
+        CorporationName = tfName.getText();
+        tablePlayer.add(tfName).minWidth(400).pad(padSize);
 
         TextButton textButtonClose = new TextButton("Next", skin);
 
@@ -62,15 +61,15 @@ public class CreatNewGameScreen extends AbstractScreen {
         ButtonGroup buttonGroup = new ButtonGroup();
         ImageTextButton imageTextButton = new ImageTextButton("Male", skin, "radio");
         buttonGroup.add(imageTextButton);
-        tablePlayer.add(imageTextButton);
+        tablePlayer.add(imageTextButton).pad(padSize);
 
         imageTextButton = new ImageTextButton("Female", skin, "radio");
         buttonGroup.add(imageTextButton);
-        tablePlayer.add(imageTextButton);
+        tablePlayer.add(imageTextButton).pad(padSize);
 
         tablePlayer.row();
         tablePlayer.row().colspan(2);
-        tablePlayer.add(textButtonClose.center().pad(10));
+        tablePlayer.add(textButtonClose.center().pad(padSize));
 
 
         textButtonClose.addListener(new ClickListener() {
@@ -78,16 +77,13 @@ public class CreatNewGameScreen extends AbstractScreen {
             @Override
             public void  touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 tablePlayer.remove();
-                try {
+
                     String filePath = locRoot;
-                    MakeXML.createPlayerFile(playerName);
+                    MakeXML.createPlayerFile(CorporationName);
                     MakeXML.createSpaceship();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
                 //game.setScreen(new SpalshScreen(game));
 
-            }
         });
 
         stage.addActor(tablePlayer);
